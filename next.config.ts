b6261293aec0bb4452335d2a2bf3f4f01f0eab90
Aditya Mali <adityamali33@gmail.com>
2025-06-24 01:24:37 +0000

    getting same error i am using feww tier would that be a reason

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  serverComponentsExternalPackages: ['@genkit-ai/googleai'],
   allowedDevOrigins: ['https://6000-firebase-studio-1750342328747.cluster-xpmcxs2fjnhg6xvn446ubtgpio.cloudworkstations.dev'],
 };
 
