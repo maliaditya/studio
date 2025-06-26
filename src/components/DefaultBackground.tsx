93a359ae512cf138b57bd29a1e7ebc5f1efdb262
Aditya Mali <adityamali33@gmail.com>
2025-06-26 18:54:21 +0000

    in debault add this ad background @import compass
        @import url(https

---
+++ src/components/DefaultBackground.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+export function DefaultBackground() {
+  return (
+    <div className="default-background-holder">
+      <div className="first"></div>
+      <div className="second"></div>
+      <div className="third"></div>
+    </div>
+  );
+}
