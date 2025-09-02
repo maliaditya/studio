38db20c93a018b074daa747a0395204312ef00ad
Aditya Mali <adityamali33@gmail.com>
2025-09-02 03:35:41 +0000

    Application Settings Modal add scrollbar in this modal and make sure the

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -61,7 +61,7 @@ const DialogHeader = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col space-y-1.5 text-center sm:text-left",
+      "flex flex-col space-y-1.5 text-center sm:text-left flex-shrink-0",
       className
     )}
     {...props}
@@ -75,7 +75,7 @@ const DialogFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 flex-shrink-0",
       className
     )}
     {...props}
