c7dde885a09fba0e6581f59e8fbf6da48fbe173c
Aditya Mali <adityamali33@gmail.com>
2025-09-03 17:20:48 +0000

    Daily Time Allocation add maximize icon on top right corner which will o

--- src/components/ProductivitySnapshot.tsx
+++ src/components/ProductivitySnapshot.tsx
@@ -513,13 +513,13 @@ export function ProductivitySnapshot({ stats, timeAllocationData, onOpenStatsMod
           {showTimeAllocation && (
             <>
                 <Separator className="my-6" />
-                 <div className="flex flex-row items-center justify-between px-6">
+                 <CardHeader className="flex flex-row items-center justify-between px-6 pt-0 pb-2">
                     <CardTitle className="flex items-center gap-2 text-primary"><PieChartIcon /> Daily Time Allocation</CardTitle>
                     <Button variant="outline" size="icon" onClick={onOpenTimeAllocationModal}>
                         <Expand className="h-4 w-4" />
                         <span className="sr-only">Open Time Allocation in Modal</span>
                     </Button>
-                </div>
+                </CardHeader>
                 <div className="flex items-center justify-center">
                   <TimeAllocationChart timeAllocationData={timeAllocationData} />
                 </div>
