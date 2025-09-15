1094ee8774a89fd6cbbc5aade079106f6a106ed0
Aditya Mali <adityamali33@gmail.com>
2025-09-15 10:04:31 +0000

    dont show the backgrounnd dotted lines

--- src/components/MindsetCategoriesCard.tsx
+++ src/components/MindsetCategoriesCard.tsx
@@ -222,7 +222,6 @@ const HourlyResistanceLogDialog = ({ isOpen, onOpenChange, allLinkedResistances 
                             data={chartData}
                             margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
                         >
-                            <CartesianGrid strokeDasharray="3 3" />
                             <XAxis dataKey="name" />
                             <YAxis allowDecimals={false} />
                             <Tooltip content={<CustomTooltip />}/>
