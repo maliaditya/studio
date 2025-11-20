99b8e1487c73395ca0b12e5a0f9e2796878dc6c8
Aditya Mali <adityamali33@gmail.com>
2025-11-20 17:56:25 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: deepWor

--- src/components/WeeklyReviewModal.tsx
+++ src/components/WeeklyReviewModal.tsx
@@ -23,7 +23,9 @@ export function WeeklyReviewModal({ isOpen, onOpenChange }: WeeklyReviewModalPro
     coreSkills,
     offerizationPlans,
     permanentlyLoggedTaskIds,
-    getDescendantLeafNodes
+    getDescendantLeafNodes,
+    deepWorkDefinitions, 
+    upskillDefinitions,
   } = useAuth();
 
   const weeklyData = useMemo(() => {
@@ -102,7 +104,7 @@ export function WeeklyReviewModal({ isOpen, onOpenChange }: WeeklyReviewModalPro
       brandingTasksCompleted,
       specializationProgress,
     };
-  }, [allUpskillLogs, allDeepWorkLogs, brandingLogs, coreSkills, offerizationPlans, permanentlyLoggedTaskIds, getDescendantLeafNodes]);
+  }, [allUpskillLogs, allDeepWorkLogs, brandingLogs, coreSkills, offerizationPlans, permanentlyLoggedTaskIds, getDescendantLeafNodes, deepWorkDefinitions, upskillDefinitions]);
 
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
